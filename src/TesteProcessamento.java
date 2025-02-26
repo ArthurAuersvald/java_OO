e01ebf4bf5624f642ea5bd75bf5d5b95d402d8b8
arthur <arthurauersvald@gmail.com>
2025-02-26 16:47:04 -0300

    Testes de processamento

---
+++ src/TesteProcessamento.java
@@ -0,0 +1,17 @@
+
+public class TesteProcessamento {
+
+	public static void main(String[] args) {
+		
+		double a;
+		int b;
+		
+		a = 5.0;
+		b = (int) a;
+		
+		System.out.println(b);
+
+
+	}
+
+}
