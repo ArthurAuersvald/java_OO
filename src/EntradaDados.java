cd0ca3d76009d7d2d48ec892d1ad0a257e871cc2
arthur <arthurauersvald@gmail.com>
2025-02-27 16:17:18 -0300

    Scanner

---
+++ src/EntradaDados.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class EntradaDados {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		String x;
+		int y;
+		double z;
+		
+		System.out.println("Digite seu nome");
+		x = sc.next();
+		System.out.println("Digite sua idade");
+		y = sc.nextInt();
+		System.out.println("Digite seu saldo");
+		z = sc.nextDouble();
+		
+		System.out.println("Seu nome é " + x);
+		System.out.println("Sua idade é " + y);
+		System.out.println("Seu saldo é " + z);
+		
+		sc.close();
+	}
+
+}
