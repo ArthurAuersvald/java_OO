5c78f212edb6937ee7f36cf8b71e29bd2aa44eca
Arthur Auersvald <arthurauersvald@gmail.com>
2025-02-27 22:22:05 -0300

    Atribuição acumulativa

---
+++ src/AtribuicaoAcumulativa.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class AtribuicaoAcumulativa {
+
+	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+
+		int minutos = sc.nextInt();
+	    
+		double conta = 50.0;
+		if (minutos > 100) {
+			conta = conta + (minutos - 100) * 2.0;
+		}
+			System.out.println("Valor da conta = R$" + conta);
+
+		sc.close();
+	}
+}
